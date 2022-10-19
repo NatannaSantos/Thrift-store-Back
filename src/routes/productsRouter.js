7e713d3bf5916a68bab89ce6938619755f419e26
Natanna <natannacortez@gmail.com>
2022-10-18 23:07:27 -0300

    fix route products

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createdProduct, getProducts, getProduct, checkout } from "../controllers/productsController.js";
 
 const productsRouter=Router();
-productsRouter.post('/Products',createdProduct);
+productsRouter.post('/products',createdProduct);
 productsRouter.get('/products',getProducts);
 productsRouter.get('/product/:id',getProduct);
 productsRouter.post('/checkout', checkout);
